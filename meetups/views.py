52675eeb2b490dcc80f82691ad02813a67e4d27f
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-09-15 21:06:37 +0530

    templates tags etc.

--- meetups/views.py
+++ meetups/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!')
+    meetups = [
+        {'title': 'First Meetup'},
+        {'title': 'Second Meetup'},
+        {'title': 'Third Meetup'}
+    ]
+    return render(request, 'meetups/static.html', 
+                  {'meetups' : meetups,
+                   'show_meetups': True}
+                  )
+
+def static(request):
+    return render(request, 'meetups/static.html')
